264eb4d98c3d2479b38d8b1326ac55db388adf50
Liferdorp <jessie__1996@hotmail.com>
2019-06-10 20:19:59 +0200

    Added files

---
+++ classes/db/class.standardvalues.php
@@ -0,0 +1,26 @@
+<?php
+
+define("defaultDataBase", "prefix_dbname");
+
+define("hostName", "hostname.com");
+
+define("MailSendUserName", "no-reply");
+
+define("MailSendUserTitle", "no-reply@hostname.com");
+
+define("MailSendHost","hostname.com");
+
+define("MailSendUserPass", "PassWordSendUsername");
+
+define("MailReplyUserName","info");
+
+define("MailReplyUserTitle", "hostname.com");
+
+define("MailSMTPSecurity","ssl"); // ssl or tls
+  
+define("MailSMTPPort","465");
+
+define("MailAltBody","Your mail client does not support HTML. Consider using another client"); // This message is shown to browsers who don't support HTML e-mails
+
+?>
+
